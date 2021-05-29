7e2f868f48a2f25ceb87b355cda4c5cb028d23d7
Podlevskiy Viktor <vitca64rus@gmail.com>
2021-05-29 16:59:38 +0300

    go to pep-8

--- avito.py
+++ avito.py
@@ -7,7 +7,6 @@ from typing import List, Tuple, Dict
 
 app = FastAPI()
 conn = sqlite3.connect('orders.db')
-key = 0
 
 
 def get_region_id(region: str) -> int:
@@ -32,25 +31,17 @@ async def get_count(key_: int, search: str, id_reg: int) -> None:
         cur = conn.cursor()
         cur.execute("INSERT INTO keys(key, count, timestamp, search_fraze, region) VALUES(?, ?, ?, ?, ?);", info)
         conn.commit()
-
-        cur.execute("SELECT * FROM keys;")
-        one_result = cur.fetchall()
-        print(one_result)
-
-        await asyncio.sleep(60)  # FIXME
+        await asyncio.sleep(60)
 
 
 @app.get("/add")
 async def root(search: str, region: str) -> Dict[str, int]:
-    #global key  # FIXME
     cur = conn.cursor()
     cur.execute("SELECT MAX(key) FROM keys;")
     key = cur.fetchone()[0] + 1
-    print(key)
     id_reg: int
     id_reg = get_region_id(region)
     asyncio.create_task(get_count(key, search, id_reg))
-    #key += 1
     return {'id связки (поисковая фраза + регион)': key}
 
 
@@ -60,5 +51,4 @@ async def root(pair_id: int, t1: int, t2: int) -> Dict[str, List[Tuple[int, int]
     sql_select_query = """select count, timestamp from keys where key = ? and timestamp > ? and timestamp < ?"""
     cur.execute(sql_select_query, (pair_id, t1, t2))
     records = cur.fetchall()
-    print('STAT', records)
     return {'счётчики и соответствующие им временные метки': records}
